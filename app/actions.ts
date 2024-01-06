fe4d25cae6b5b0619f92019487b814ac0b6b9ba2
Mark Robertson <mark.rh.robertson@gmail.com>
2024-01-06 16:29:42 +0000

    Attemping to fix Vercel KV.

--- app/actions.ts
+++ app/actions.ts
@@ -27,18 +27,20 @@ const TIME_CONVERSION: { [id: string]: number } = {
   w: 604800,
 };
 
+const REDIS_CFG = {
+  url: process.env.REDIS_URL
+    ? process.env.REDIS_URL
+    : process.env.KV_URL
+    ? process.env.KV_URL
+    : "redis://localhost:6379",
+  socket: { tls: process.env.NO_SSL ? false : true },
+};
+
 export async function submit(payload: SubmitPayload) {
   const key = new Uint8Array(randomBytes(32));
   const furnace = new Furnace(key);
   const token = furnace.encode(payload.content);
-  console.log(`Key: ${toBase64URL(key)} | Token: ${toBase64URL(token)}`);
-  const client = createClient({
-    url: process.env.REDIS_URL
-      ? process.env.REDIS_URL
-      : process.env.KV_URL
-      ? process.env.KV_URL
-      : "redis://localhost:6379",
-  });
+  const client = createClient(REDIS_CFG);
   const uuid = randomUUID();
   const keyB64 = toBase64URL(key);
   await client.connect();
@@ -57,13 +59,7 @@ export async function submit(payload: SubmitPayload) {
 export async function revealSecret(payload: RevealPayload) {
   const key = toUint8Array(payload.key);
   const furnace = new Furnace(key);
-  const client = createClient({
-    url: process.env.REDIS_URL
-      ? process.env.REDIS_URL
-      : process.env.KV_URL
-      ? process.env.KV_URL
-      : "redis://localhost:6379",
-  });
+  const client = createClient(REDIS_CFG);
   await client.connect();
   const res = await client.get(`tempest:${payload.uuid}`);
   await client.del(`tempest:${payload.uuid}`);
