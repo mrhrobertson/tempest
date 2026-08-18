5fdb9a56af58ebcece901e0901c9066a7cd72a46
Mark Robertson <61762824+mrhrobertson@users.noreply.github.com>
2026-08-18 20:34:21 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: { config: "./config/tailwind.config.ts" },
+    "@tailwindcss/postcss": { config: "./config/tailwind.config.ts" },
     autoprefixer: {},
   },
 };
